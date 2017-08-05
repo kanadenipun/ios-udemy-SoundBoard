2ae1a7bcf35d23db24b32d52d92358e32b4195a8
kanadenipun <nipunk@thoughtworks.com>
2017-08-05 12:02:44 +0530

    Added UI Outlets for buttons

--- SoundBoard/ViewController.swift
+++ SoundBoard/ViewController.swift
@@ -10,16 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordButton: UIButton!
+    
+    @IBOutlet weak var playButton: UIButton!
+    
+    @IBOutlet weak var fileNameTextField: UITextField!
+    
+    @IBOutlet weak var saveButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+  
+    @IBAction func recordButtonTapped(_ sender: Any, forEvent event: UIEvent) {
     }
-
-
+    
+    @IBAction func playButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func saveButtonTapped(_ sender: Any) {
+    }
+    
 }
 
